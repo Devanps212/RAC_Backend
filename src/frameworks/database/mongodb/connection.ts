ed2991f778ae325d5329f00973605c90b463f55b
Devanps <devanps212@gmail.com>
2024-06-26 09:58:12 +0530

    rebuild 1

--- src/frameworks/database/mongodb/connection.ts
+++ src/frameworks/database/mongodb/connection.ts
@@ -4,7 +4,7 @@ import configFile from '../../../config';
 
 const connectDb = async () => {
   console.log('Attempting to connect to the database...');
-  console.log("mongo uri : ", configFile.MONGO_URL)
+  
 
   try {
 
