19650c56c8d56613ec2bf223cf84406b85f425c2
Devanps <devanps212@gmail.com>
2024-06-26 14:13:07 +0530

    sample bug fix again

--- src/app/use_case/user/user.ts
+++ src/app/use_case/user/user.ts
@@ -14,7 +14,7 @@ export const locationFinder = async(data: string)=>{
         console.log("Data ===>",data)
         console.log(configFile.SEARCH_LOCATION)
         console.log(process.env)
-        const response = await axios.get(`${configFile.SEARCH_LOCATION}suggest?q=${data}&access_token=${configFile.LOCATION_ACCESS_TOKEN}&session_token=ce8adf6d-f635-415e-ad83-7597a752bdfc&language=en&limit=10&types=country%2Cregion%2Cdistrict%2Cpostcode%2Clocality%2Cplace%2Cneighborhood%2Caddress%2Cpoi%2Cstreet%2Ccategory&proximity=76.3218144%2C9.9380786`)
+        const response = await axios.get(`https://api.mapbox.com/search/searchbox/v1/suggest?q=${data}&access_token=${configFile.LOCATION_ACCESS_TOKEN}&session_token=ce8adf6d-f635-415e-ad83-7597a752bdfc&language=en&limit=10&types=country%2Cregion%2Cdistrict%2Cpostcode%2Clocality%2Cplace%2Cneighborhood%2Caddress%2Cpoi%2Cstreet%2Ccategory&proximity=76.3218144%2C9.9380786`)
         return response.data.suggestions
     }
     catch(error:any)
